34dff5b04a9b759602e7eb374af9d25783b2d8be
Juless97 <70093036+PancakeMan-py@users.noreply.github.com>
2022-08-11 04:36:55 +0100

    leetcode_4 Isomorphic

--- src/AddTwoNum/AddTwoNum.java
+++ src/AddTwoNum/AddTwoNum.java
@@ -2,15 +2,15 @@ package AddTwoNum;
 
 public class AddTwoNum {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //sentinal node
-        ListNode sentinal = new ListNode(-1);
+        //sentinel node
+        ListNode sentinel = new ListNode(-1);
         //if a carry needed
         boolean carry = false;
         //sum of two node
         int sum ;
         int iniSum ;
         //irritated pointer
-        ListNode node = sentinal;
+        ListNode node = sentinel;
 
         while (l1 != null || l2 != null) {
             int val1 = l1 == null ? 0 : l1.val;
@@ -20,6 +20,7 @@ public class AddTwoNum {
             sum = iniSum % 10;
             int finalSum = sum;
             carry = iniSum >= 10;
+            //sentinel node changed from -1,null to -1,next
             node.next = new ListNode(finalSum);
             node = node.next;
 
@@ -35,6 +36,6 @@ public class AddTwoNum {
             node.next = new ListNode(1);
         }
 
-        return sentinal.next;
+        return sentinel.next;
     }
 }
---
+++ src/IsIsomorphic/isIsomorphic.java
@@ -0,0 +1,64 @@
+package IsIsomorphic;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class isIsomorphic {
+    public boolean isIsomorphicMethod(String s, String t) {
+        Map<Character, Character> mapS = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            Character charS = s.charAt(i);
+            Character charT = t.charAt(i);
+            if(mapS.containsKey(charS)){
+                if(mapS.get(charS) != charT){
+                    return false;
+                }
+            }
+            mapS.put(charS,charT);
+        }
+
+        Map<Character, Character> mapT = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            Character charS = s.charAt(i);
+            Character charT = t.charAt(i);
+            if(mapT.containsKey(charT)){
+                if(mapT.get(charT) != charS){
+                    return false;
+                }
+            }
+            mapT.put(charT,charS);
+        }
+        return true;
+
+    }
+
+    public boolean isIsomorphic2(String s, String t) {
+        for(int i = 0; i < s.length(); i++){
+            if(s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    void isIsomorphicMethodTest() {
+        String s = "egg";
+        String t = "app";
+        boolean result = isIsomorphicMethod(s,t);
+        assertEquals(true,result);
+    }
+
+    @Test
+    void isIsomorphic2Test() {
+        String s = "egg";
+        String t = "app";
+        boolean result = isIsomorphic2(s,t);
+        assertEquals(false,result);
+    }
+}
+
