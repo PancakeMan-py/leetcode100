e9bcfcac73bb546292beaf4c8ba9a58327df24b9
Juless97 <70093036+PancakeMan-py@users.noreply.github.com>
2022-08-12 23:47:38 +0100

    leetcode_5 isSubsequence DP

--- src/isSubsequence/isSubsequence.java
+++ src/isSubsequence/isSubsequence.java
@@ -8,20 +8,32 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class isSubsequence {
-    public boolean isSubsequenceMethod(String s, String t) {
-        int i = 0,j = 0;
-        while(i<s.length()&&j<t.length()){
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
+        public boolean isSubsequenceMethod(String s, String t) {
+            int n = s.length(), m = t.length();
+
+            int[][] f = new int[m + 1][26];
+            for (int i = 0; i < 26; i++) {
+                f[m][i] = m;
             }
-            j++;
+
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = 0; j < 26; j++) {
+                    if (t.charAt(i) == j + 'a')
+                        f[i][j] = i;
+                    else
+                        f[i][j] = f[i + 1][j];
+                }
+            }
+            int add = 0;
+            for (int i = 0; i < n; i++) {
+                if (f[add][s.charAt(i) - 'a'] == m) {
+                    return false;
+                }
+                add = f[add][s.charAt(i) - 'a'] + 1;
+            }
+            return true;
         }
 
-        if(i<s.length()){
-            return false;
-        }
-        return true;
-    }
 
     @Test
     void isSubsequenceTest(){
